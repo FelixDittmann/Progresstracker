958848914e0c1ae4a8c1abbb70db398ce803d6e3
felix <felix-dittmann@gmx.de>
2024-10-03 17:32:39 +0200

    feat: Started implementing database for game achievements as first use case of the progress tracker

---
+++ Database/ProgressTrackerDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progresstracker.Database
+{
+
+public class ProgressTrackerDatabaseContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Achievement> Achievements { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // SQLite-Datenbankverbindung konfigurieren
+            optionsBuilder.UseSqlite("Data Source=steam.db");
+        }
+    }
+}
---
+++ Database/ProgressTrackerDatabaseModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace Progresstracker.Database
+{
+    using System.Collections.Generic;
+
+    public class Game
+    {
+        public int Id { get; set; }
+        public int AppId { get; set; }
+        public string Name { get; set; }
+        public int PlaytimeForever { get; set; }
+
+        // Navigation Property: Eine Liste von Errungenschaften pro Spiel
+        public List<Achievement> Achievements { get; set; }
+    }
+
+    public class Achievement
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Achieved { get; set; }
+
+        // Fremdschlüssel zum Spiel
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+    }
+    internal class ProgressTrackerDatabaseModel
+    {
+    }
+}
