01160fae12001a9c0f9567f81ac00ff8ba6c5e52
felix <felix-dittmann@gmx.de>
2024-10-05 14:13:37 +0200

    feat: Added Configuration handling. Will be extended in future development

---
+++ Configuration/ConfigurationSettingsHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progresstracker.Configuration
+{
+    public class ConfigurationSettingsHandler
+    {
+        public readonly string DatabasePath;
+
+        public ConfigurationSettingsHandler()
+        {
+            DatabasePath = System.Configuration.ConfigurationManager.AppSettings["DbPath"] ?? "";
+        }
+    }
+}
