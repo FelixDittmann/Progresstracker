0325d072d50e99c0dcf16b8ec45d04e7083e59a6
felix <felix-dittmann@gmx.de>
2024-10-12 17:59:06 +0200

    feat: Added activities to data objects . Deleted Spiel class because game class does the same.

---
+++ DataObjects/Profile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progresstracker.DataObjects
+{
+    public class Profile
+    {
+        public int ProfileID { get; set; }
+        public string Name { get; set; }
+        public string SteamApiKey { get; set; }
+        public string SteamProfileID { get; set; }
+        public List<string> Sources { get; set; }
+    }
+}
---
+++ DataObjects/SportActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progresstracker.DataObjects
+{
+    public class SportActivity(string name, string category, int wantedResult, int? currentProgress) : IActivity
+    {
+        public long ActivityID { get; set; }
+        public string Name { get; set; } = name;
+        public string Category { get; set; } = category;
+        public int WantedResult { get; set; } = wantedResult;
+        public int CurrentProgress { get; set; } = currentProgress ?? 0;
+
+        public void UpdateCurrentProgress(int currentProgress)
+        {
+            CurrentProgress = currentProgress;
+        }
+    }
+}
