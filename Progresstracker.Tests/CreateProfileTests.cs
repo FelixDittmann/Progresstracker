0ddc79211c9dd1b5290a4072145875e9d942ce4a
JanikDa <janik.dauner@gmail.com>
2025-05-28 11:32:48 +0200

    Setup for Unit Tests

---
+++ Progresstracker.Tests/CreateProfileTests.cs
@@ -0,0 +1,24 @@
+namespace Progresstracker.Tests
+{
+    public class CreateProfileTests
+    {
+        [Fact]
+        public void CreateProfile_NoInput()
+        {
+            //TODO
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void CreateProfile_WrongInput()
+        {
+            //TODO
+        }
+
+        [Fact]
+        public void CreateProfile_ValidInput()
+        {
+            //TODO
+        }
+    }
+}
